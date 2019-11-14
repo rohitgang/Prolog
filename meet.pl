9bd1ef3cf31fd634413a363ecf539d92d25199e1
Rohit Gangurde <rohitgangurde@u.boisestate.edu>
2019-11-13 22:56:52 -0700

    Update meet.pl

--- meet.pl
+++ meet.pl
@@ -30,18 +30,5 @@ people([ann,bob,carla]).
 main :-findall(Slot,meet(Slot),Slots),
        uniq(Slots,Uniq),
        write(Uniq),nl,halt.
-/*meet(Person, slot(time(U,V), time(X,Y))) :-
-    free(Person, slot(time(A,B), time(D,E))),
-    J is V+(U*60), K is Y+(X*60),
-    L is B+(A*60), M is E+(D*60),
-    L=<J,
-    M>=K.*/
 
-
-
-/*main :- meet(Person,
-        slot(time(8,30), 
-        time(8,45))), 
-        write(Person),nl,halt.
-*/
 :- initialization(main).
