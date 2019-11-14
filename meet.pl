5ba58ed98fff9c9c6e40fc7e10583eeb6e6578e3
ROHIT <rohitgangurde@u.boisestate.edu>
2019-11-13 18:41:26 -0700

    Ann works

--- meet.pl
+++ meet.pl
@@ -1,4 +1,7 @@
-:- include('data.pl')
+#!/bin/gprolog --consult-file
+
+:- include('data.pl').
+:- include('uniq.pl').
 
 meet(Person, slot(time(U,V), time(X,Y))) :-
     free(Person, slot(time(A,B), time(D,E))),
@@ -6,7 +9,14 @@ meet(Person, slot(time(U,V), time(X,Y))) :-
     L is B+(A*60), M is E+(D*60),
     L=<J,
     M>=K.
+people([ann,bob, carla]).
 
-main :- meet(Person, slot(time(8,30), time(8,45))), write(Person),nl,halt.
+/*main :- findall(Slot, meet(Slot), Slots),
+        uniq(Slots, Uniq),
+        write(Uniq), nl, halt.*/
+main :- meet(Person,
+        slot(time(8,30), 
+        time(8,45))), 
+        write(Person),nl,halt.
 
-:- initialization(main)
+:- initialization(main).
